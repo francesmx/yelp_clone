ca678f26eab4fc027a0a7b7b21ddf1cab1fb88f0
Frances Maxwell <fmaxwell@gmail.com>
2016-11-17 17:40:03 +0000

    Won't let you review unless signed in. Tweaked restaurant view.

--- spec/features/review_features_spec.rb
+++ spec/features/review_features_spec.rb
@@ -13,7 +13,6 @@ feature "Reviews" do
     fill_in "rating", with: "2"
     click_button "Review"
     expect(page).to have_content "Pretty average place. Needs improvement"
-    expect(page).to have_content "Rated 2/5"
   end
 
 end
